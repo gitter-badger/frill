577a997286b3184345bcaea84b6348881d442164
kotorieclair <kotorieclair@gmail.com>
2015-08-03 12:52:50 +0900

    added title

--- src/components/Top/index.jsx
+++ src/components/Top/index.jsx
@@ -29,7 +29,7 @@ class Top extends new StoreWatchComponent(['Test']) {
   render() {
     return (
       <div>
-        This is Top component<br />
+        <h2>FrillJS</h2>
         <h4>{this.state.count}</h4>
           <button onClick={this.onOne}>+1</button>
           <button onClick={this.onTen}>+10</button>
