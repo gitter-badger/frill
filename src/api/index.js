704c75bcacfa37c2e3215875675910b4a47c9005
nanopx <scy.a0217@gmail.com>
2015-08-05 00:21:56 +0900

    fixed routePrefixer and tagged for docs

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,4 @@
 import routes from './routes';
-// import {each as _each} from 'lodash';
-// const prefixLoop = (apiRoutes) => {
-//   const prefixedRoutes = [];
-//   const loop = (_routes, _prefixed) => {
-//     _each(_routes, (route, prefix) => {
-//       let _prefix = _prefixed;
-//       if (typeof prefix === 'string') {
-//         _prefix = !_prefixed ? prefix : _prefixed + prefix;
-//       }
-//       if (route.config) {
-//         route.path = _prefix + route.path;
-//         prefixedRoutes.push(route);
-//       } else {
-//         loop(route, _prefix);
-//       }
-//     });
-//   };
-//   loop(apiRoutes);
-//   return prefixedRoutes;
-// };
-
 
 const api = (server) => {
   const {apiRoutes, authRoutes} = routes(server);
--- src/helpers/routePrefixer.js
+++ src/helpers/routePrefixer.js
@@ -1,8 +1,28 @@
 import {isArray as _isArray} from 'lodash';
+import {isObject as _isObject} from 'lodash';
 
-export default (prefix, routes) => {
+/**
+ * @desc Prefixes route.path
+ * @example
+ * routePrefixer('/api', [{path: '/a'}, {path: '/b'}]);
+ *
+ * @param {string} prefix - A string to prefix paths
+ * @param {Array|Object} routes - Collection of routes or a single route
+ * @param {string} route.path - Collection of routes or a single route
+ * @throws {Error} throw error when route.path doesn't exist
+ * @throws {TypeError} throw error when route is not an Array
+ * @return {Array}
+ */
+export default routePrefixer = (prefix, routes) => {
+  let _routes = routes;
+  if (_isObject(routes)) {
+    if (!routes.path) {
+      throw new Error('A route must have routes.path');
+    }
+    _routes = [_routes];
+  }
   if (!_isArray(routes)) {
-    throw new TypeError('Unable to prefix routes. route must be an Array.');
+    throw new TypeError('Unable to prefix routes. route must be an Array');
   }
   return routes.map((route) => {
     route.path = prefix + route.path;
