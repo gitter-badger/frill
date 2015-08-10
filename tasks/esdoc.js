5825558bae5772d3c15f6f34e18c99bf0be27f49
吉武篤司 <0nanopx@gmail.com>
2015-08-10 09:51:10 +0900

    moved esdoc configurations to esdoc.json

--- tasks/esdoc.js
+++ tasks/esdoc.js
@@ -9,32 +9,14 @@
  * Auto-generate documents using ESDoc.
  *
  */
+
+import config from '../esdoc';
+
 export default (gulp, $, argv, path) => {
   gulp.task('doc', () => {
+    console.log(config);
     return gulp.src(['./src'])
-      .pipe($.esdoc({
-        destination: './docs',
-        package: './package.json',
-        index: './README_DOC.md',
-        title: 'FrillJS',
-        includes: ['\\.(js|jsx)$'],
-        coverage: true,
-        // excludes: ['\\.config\\.(js|jsx)$'],
-        // access: ['public', 'protected'],
-        // autoPrivate: true,
-        // unexportIdentifier: false,
-        // undocumentIdentifier: true,
-        // builtinExternal: true,
-        // importPathPrefix: '',
-        test: {
-          type: 'mocha',
-          source: './test',
-          includes: ['Test\\.(js|jsx)$'],
-          excludes: ['\\.config\\.(js|jsx)$'],
-        },
-        // styles: ['./path/to/style.css'],
-        // scripts: ['./path/to/script.js']
-      }));
+      .pipe($.esdoc(config));
   });
 
   gulp.task('doc:open', () => {
