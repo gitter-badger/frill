da9257a130687cc58b98a57206adfc4f54548890
kotorieclair <kotorieclair@gmail.com>
2015-08-11 12:29:57 +0900

    added favicons

--- tasks/assets.js
+++ tasks/assets.js
@@ -11,25 +11,12 @@
  *
  */
 export default (gulp, $, argv) => {
-  gulp.task('assets:images', () => {
-    const images = ['./src/assets/images/**'];
-
-    return gulp.src(images)
+  gulp.task('assets', () => {
+    return gulp.src('src/assets/**/*')
       .pipe($.changed('public'))
       .pipe($.imagemin())
       .pipe(gulp.dest('public'))
-      .pipe($.if(!argv.silent, $.size({title: 'assets:images'})))
+      .pipe($.if(!argv.silent, $.size({title: 'assets'})))
       .pipe($.if($._watch, $.browserSync.reload));
   });
-
-  gulp.task('assets:vendor', () => {
-    const vendor = ['./src/assets/vendor/**'];
-
-    return gulp.src(vendor)
-      .pipe($.changed('public'))
-      .pipe(gulp.dest('public'))
-      .pipe($.if($._watch, $.browserSync.reload));
-  });
-
-  gulp.task('assets', ['assets:images', 'assets:vendor']);
 };
