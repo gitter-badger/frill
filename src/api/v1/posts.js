1abe02eb9f586aee431e28f53730c95601e23038
nanopx <scy.a0217@gmail.com>
2015-08-13 01:10:51 +0900

    added hapi-holiday as an external package

--- src/api/v1/posts.js
+++ src/api/v1/posts.js
@@ -7,80 +7,18 @@ import Holiday from '../Holiday';
 const posts = (server, Models) => {
   const {mysql} = Models;
 
-  Holiday.define('sequelize', {
-    index: (Model, request, done) => {
-      Model.findAll({
-        offset: request.query.offset,
-        limit: request.query.limit,
-      })
-      .then((records) => {
-        done(null, records);
-      })
-      .catch((err) => {
-        done(err, null);
-      });
-    },
-
-    show: (Model, request, done) => {
-      Model.findById(request.params.id)
-      .then((records) => {
-        done(null, records);
-      })
-      .catch((err) => {
-        done(err, null);
-      });
-    },
-
-    save: (Model, request, done) => {
-      Model.create(request.payload).then((result) => {
-        done(null, result);
-      })
-      .catch((err) => {
-        done(err, null);
-      });
-    },
-
-    update: (Model, request, done) => {
-      Model.update(request.payload, {
-        where: {
-          id: request.params.id,
-        },
-      })
-      .then((result) => {
-        done(null, result);
-      })
-      .catch((err) => {
-        done(err, null);
-      });
-    },
-
-    destroy: (Model, request, done) => {
-      Model.destroy({
-        where: {
-          id: request.params.id,
-        },
-      })
-      .then((affected) => {
-        done(null, affected);
-      })
-      .catch((err) => {
-        done(err, null);
-      });
-    },
-  });
-
   // Holiday.configure({
   //   tags: ['api'],
   // });
 
   const PostApi = new Holiday('sequelize', 'posts', mysql.Posts);
 
-  PostApi.validate().save({
+  PostApi.validate.save({
     title: Joi.string().required(),
     content: Joi.string().required(),
   });
 
-  PostApi.validate().update({
+  PostApi.validate.update({
     title: Joi.string(),
     content: Joi.string(),
   });
